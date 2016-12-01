2bc9d4758727ca4225f27411229491f64454ad5d
mato9 <mato9@users.noreply.github.com>
2016-12-01 22:00:02 +0100

    Add files via upload

---
+++ app/application/controllers/menu.php
@@ -0,0 +1,95 @@
+<?php
+    class Menu extends CI_Controller {
+
+            /**
+             * Inser constructor.
+             */
+            public function __construct() {
+                parent::__construct();
+                $this->load->library('form_validation');
+                $this->load->model("menu/m_menu");
+            }
+
+            /**
+             *  Default method while requesting this controller
+             */
+            public function index() {
+                //$this->load->view("menu/v_menu");
+                $this->show_food();
+            }
+
+            function show_food() {
+            $id = $this->uri->segment(3);
+
+            $data['foods'] = $this->m_menu->display_food();
+            $data['single_food'] = $this->m_menu->display_food_id($id);
+
+            $this->load->view('menu/v_menu', $data);
+            }
+
+            public function new_menu()
+            {
+                $this->form_validation->set_rules('name', 'Food', 'trim|required|xss_clean');
+                $this->form_validation->set_rules('price', 'Price', 'trim|required|xss_clean');
+                $this->form_validation->set_rules('ingredience', 'Ingredience', 'trim|required|xss_clean');
+                $this->form_validation->set_rules('quantity', 'Quantity', 'trim|required|xss_clean');
+                $data = array(
+                    'id_food' => 'NULL', // AUTO-INCREMENT
+                    'name' => $this->input->post('name'),
+                    'price' => $this->input->post('price'),                    
+                    //'weight' => $this->input->post('food_weigh'),
+                    'quantity' => $this->input->post('quantity'),
+                    'ingredience' => $this->input->post('ingredience')
+                );
+                
+                $inserting_food = $this->m_menu->insert_food($data);
+               
+                if ($inserting_food == TRUE) {
+                    $data['message_display'] = 'Adding food successful !';
+                    $this->load->view('v_admin_page', $data);
+                } else {
+                    $data['message_display'] = 'Error, food already exist!';
+                    echo "<script type='text/javascript'>
+                        alert('Error, food already exist!');
+                        </script>";
+                    //$this->load->view('menu/v_menu', $data);
+                    $this->show_food();
+                }
+            }
+
+            function update_menu_name() {
+                $id = $this->input->post('food_name');
+                $this->input->post('delete');
+                if($this->input->post('update')){
+                    $data = array(
+                        'name' => $this->input->post('food_name'),
+                        'price' => $this->input->post('food_price'),
+                        //'weight' => $this->input->post('food_weigh'),
+                        'quantity' => $this->input->post('food_quantity'),
+                        'ingredience' => $this->input->post('food_ingredience')
+                    );
+                    $result = $this->m_menu->update_menu($id,$data);
+                    if($result)
+                    {
+                        $data['message_display'] = 'Update Successfully !';
+                    }
+                    else {
+                        $data['message_display'] = 'Update Error !';
+                        echo "<script type='text/javascript'>
+                        alert('Update error');
+                        </script>";
+                    }
+                    $this->show_food();
+                }
+                else{
+                    $result = $this->m_menu->delete_menu($id);
+                    $data['message_display'] = 'Delete Successfully !';
+                    echo "<script type='text/javascript'>
+                        alert('Delete Successfully !');
+                        </script>";
+                    $this->show_food();
+                }
+            }
+
+    }
+?>
---
+++ app/application/controllers/update.php
@@ -0,0 +1,66 @@
+<?php
+    class Update extends CI_Controller{
+        function __construct(){
+            parent::__construct();
+            $this->load->model('m_update');
+        }
+
+        public function index() {
+           // $this->load->view("v_update");
+            $this->show_employee_id();
+        }
+
+        function show_employee_id() {
+            $id = $this->uri->segment(3);
+            $data['employee'] = $this->m_update->show_employee();
+            $data['single_login'] = $this->m_update->show_employee_id($id);
+            /*if($data){
+                var_dump($data);
+            }*/
+            $this->load->view('v_update', $data);
+        }
+        function update_employee_id1() {
+            $id = $this->input->post('login');
+            if($this->input->post('update')){
+                $data = array(
+                        'firstname' => $this->input->post('firstname'),
+                        'lastname' => $this->input->post('lastname'),
+                        'email' => $this->input->post('email'),
+                        'birthdate' => date('Y-m-d', strtotime($this->input->post('birthdate'))),
+                        'contract_from_date' => $this->input->post('contract_from_date'),
+                        'contract_to_date' => $this->input->post('contract_to_date'),
+                        'address' => $this->input->post('address'),
+                        'phone' => $this->input->post('phone')
+                );
+                $result = $this->m_update->update_employee_id1($id,$data);
+                if($result)
+                {
+                    $data['message_display'] = 'Update Successfully !';
+                    //$this->load->view('v_login_form', $data);
+                }
+                else {
+                    $data['message_display'] = 'Update error!';
+                }
+                $this->show_employee_id();
+            }
+            else{
+                $result = $this->m_update->delete_employee($id);
+                if($result)
+                {
+                    $data['message_display'] = 'Delete Successfully !';
+                    echo "<script type='text/javascript'>
+                    alert('Delete employee successfully');
+                    </script>";
+                    //$this->load->view('v_login_form', $data);
+                }
+                else {
+                    $data['message_display'] = 'Delete error!';
+                    echo "<script type='text/javascript'>
+                    alert('Delete employee error');
+                    </script>";
+                }
+                $this->show_employee_id();
+            }
+        }
+    }
+?>
