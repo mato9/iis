51957eb18168e56ae59108296bf929e6cb914cba
mato9 <mato9@users.noreply.github.com>
2016-12-01 22:07:27 +0100

    Add files via upload

--- app/application/models/menu/m_menu.php
+++ app/application/models/menu/m_menu.php
@@ -1,1 +1,58 @@
+<?php
+	class M_Menu extends CI_Model {
 
+    
+        public function display_food() {
+            $this->db->select('*');
+            $this->db->from('menu');
+
+            $query = $this->db->get();
+            return $query->result();
+        }
+
+        public function display_food_id($data) {
+            $condition = "name = "."'".$data."'";
+            $this->db->select('*');
+            $this->db->from('menu');
+            $this->db->where($condition);
+            $query = $this->db->get();
+            return $query->result();
+        }
+
+
+
+        public function insert_food($data) {
+
+            $condition = "name =" . "'" . $data['name'] . "'";
+
+            $this->db->select('*');
+            $this->db->from('menu');
+            $this->db->where($condition);
+            $this->db->limit(1);
+            $query = $this->db->get();
+
+            if ($query->num_rows() == 0) {
+                $this->db->insert('menu', $data);
+                if ($this->db->affected_rows() > 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+		}
+
+        public function update_menu($id, $data){
+            $this->db->where('name', $id);
+            $this->db->update('menu', $data);
+            return true;
+        }
+
+        public function delete_menu($id){
+            $this->db->where('name', $id);
+            $this->db->delete('menu');
+            return true;
+        }
+
+}
