823ac7df4d387d8f12c64b6e5702efaccdb3d9cf
mato9 <mato9@users.noreply.github.com>
2016-12-02 03:38:47 +0100

    Add files via upload

--- app/application/models/order/m_order.php
+++ app/application/models/order/m_order.php
@@ -1,1 +1,48 @@
+<?php
+	class M_order extends CI_Model {
 
+    
+        public function display_order() {
+            $this->db->select('*');
+            $this->db->from('menu_orders');
+
+            $query = $this->db->get();
+
+            return $query->result();
+        }
+
+        public function display_order_id($data) {
+            $condition = "id_table = " . "'" . $data. "'";
+            $this->db->select('*');
+            $this->db->from('menu_orders');
+            $this->db->where($condition);
+
+            $query = $this->db->get();
+
+            return $query->result();
+        }
+
+        public function get_employee($id) {
+            //echo $id."  ";
+            $condition = "id =" . "'" . $id . "'";
+            $this->db->select('login');
+            $this->db->from('employee');
+            $this->db->where($condition);
+            $this->db->limit(1);
+            $query = $this->db->get();
+            $login = $query->result();
+            foreach($login as $loginn){ foreach($loginn as $log)  return $log;}
+        }
+
+        public function get_menu($id) {
+            $condition = "id_food =" . "'" . $id . "'";
+            $this->db->select('name');
+            $this->db->from('menu');
+            $this->db->where($condition);
+            $this->db->limit(1);
+            $query = $this->db->get();
+            $login = $query->result();
+            foreach($login as $loginn){ foreach($loginn as $log)  return $log;}
+        }
+
+}
